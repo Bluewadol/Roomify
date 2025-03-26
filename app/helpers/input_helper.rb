5bb6af5c1eaf06c1523ebe798cdeca96d112cc00
Bhuwadol <ttthisriton29@gmail.com>
2025-03-26 21:32:26 +0700

    [no-id] update color in input helper

--- app/helpers/input_helper.rb
+++ app/helpers/input_helper.rb
@@ -4,9 +4,9 @@ module InputHelper
         base_classes += " pl-12" if has_icon_left
 
         if resource.errors[field].any?
-            base_classes += " border-red-700 text-red-700 ring-red-50 dark:border-red-400 dark:text-red-400 dark:ring-red-50/10"
+            base_classes += " border-rose-700 text-rose-700 ring-red-50 dark:border-red-400 dark:ring-red-50/10"
         else
-            base_classes += " invalid:border-red-700 invalid:text-red-700 invalid:ring-red-50 dark:invalid:border-red-400 dark:invalid:text-red-400 dark:invalid:ring-red-50/10"
+            base_classes += " invalid:border-rose-700 invalid:text-red-700 invalid:ring-red-50 dark:invalid:ring-red-50/10"
         end
 
         base_classes
@@ -15,7 +15,7 @@ module InputHelper
     def error_icon(resource, field)
         return unless resource.errors[field].any?
 
-        content_tag(:span, class: "absolute right-3 top-3 text-red-700 dark:text-red-400") do
+        content_tag(:span, class: "absolute right-3 top-3 text-red-700") do
             icon("exclamation-circle", class: "size-5 stroke-current")
         end
     end
@@ -23,7 +23,7 @@ module InputHelper
     def field_error_tag(resource, field)
         return unless resource.errors[field].any?
 
-        content_tag(:p, resource.errors[field].first, class: "mt-1 text-sm text-red-700 dark:text-red-400")
+        content_tag(:p, resource.errors[field].first, class: "mt-1 text-sm text-rose-700")
     end
 
     def error_messages(resource)
