21351d6e0dd1793a145cc1e7452f8db11bf38823
Bhuwadol <ttthisriton29@gmail.com>
2025-03-30 18:04:17 +0700

    [RMFB-019] update Alert notification and a flash effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,22 @@ module.exports = {
     rails_ui_template_path
   ],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        'toast-from-right': {
+          '0%': { transform: 'translateX(50%)', opacity: '0%' },
+          '100%': { transform: 'translateX(0)', opacity: '100%' },
+        },
+        'toast-from-left': {
+          '0%': { transform: 'translateX(-50%)', opacity: '0%' },
+          '100%': { transform: 'translateX(0)', opacity: '100%' },
+        },
+      },
+      animation: {
+        'toast-from-right': 'toast-from-right 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55)',
+        'toast-from-left': 'toast-from-left 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55)',
+      },
+    }
   },
   plugins: []
 }
