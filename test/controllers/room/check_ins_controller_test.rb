48c0e2578103521ad3867806a83134f724bc847b
Bhuwadol <ttthisriton29@gmail.com>
2025-04-19 00:51:36 +0700

    fix test

--- test/controllers/room/check_ins_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Room::CheckInsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/reservation_member_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ReservationMemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
