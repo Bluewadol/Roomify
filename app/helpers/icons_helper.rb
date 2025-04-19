06f251bb5223f1f1836ee4b81934c35dbaab3b75
Bhuwadol <ttthisriton29@gmail.com>
2025-04-19 16:16:16 +0700

    fix logo

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -8,7 +8,7 @@ module IconsHelper
   end
   def render_icon_file(filename, **options)
     filtered_options = options.slice(*VALID_SVG_ATTRIBUTES)
-    svg_content = Rails.root.join("app/assets/icons/#{filename}.svg").read
+    svg_content = Rails.root.join("app/assets/images/#{filename}.svg").read
     svg_content.sub!("<svg", "<svg #{tag.attributes(filtered_options)} ")
     raw(svg_content)
   end
