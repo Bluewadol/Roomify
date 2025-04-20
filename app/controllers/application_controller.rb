658477ad645b808827fec5b0a0c9753e58b8eb2f
Bhuwadol <ttthisriton29@gmail.com>
2025-04-20 16:53:00 +0700

    fix public pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,8 @@ class ApplicationController < ActionController::Base
   private
 
   def public_page?
-    request.path == root_path
+    request.path == rooms_path || 
+    request.path.start_with?('/rooms/')
   end
 
   def set_user
